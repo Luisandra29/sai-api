7692b810ba832e56c74f13165f0281af96415668
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-06-15 15:29:49 -0400

    Remove old frontend

--- admin/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import dialogReducer from './dialogReducer';
-import userReducer from './userReducer';
-import sidebarReducer from './sidebarReducer';
-
-export default {
-    user: userReducer,
-    dialog: dialogReducer,
-    sidebar: sidebarReducer
-};
